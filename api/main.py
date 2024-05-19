645622ff2d7bcf14d7afcc808e53f62f40a0a360
endymion18 <taciturnasilentia@gmail.com>
2024-05-20 00:04:36 +0500

    feat: add new endpoints

--- api/main.py
+++ api/main.py
@@ -1,13 +1,52 @@
+import json
+import os
+import smtplib
+from email.message import EmailMessage
+from pathlib import Path
+
 from fastapi import FastAPI
+from starlette.responses import FileResponse
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+with open("../configs/email.json", "r") as f:
+    email_config = json.load(f)
 
 
-@app.get("/users")
-async def abc():
-    return "all users"
+@app.get("/ping")
+async def ping():
+    return "pong"
+
+
+@app.get("/media/{path}")
+async def get_avatar(path: str):
+    os.chdir(".")
+    img_path = Path(f"media/{path}")
+    print(img_path)
+    return FileResponse(img_path)
+
+
+@app.get("/help")
+async def send_email():
+    msg = EmailMessage()
+    msg['Subject'] = 'Необходима помощь'
+    msg['From'] = email_config["sender"]
+    msg['To'] = email_config["receiver"]
+
+    msg.set_content("Подойдите, пожалуйста, к информационному стенду")
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+        server.login(email_config["sender"], email_config["sender_password"])
+        server.send_message(msg)
+
+    return {"success": "Email has been sent"}
+
+
+@app.get("/search")
+async def search(info: str):
+    return None
+
+
+@app.get("/get-all-sections")
+async def get_all_sections():
+    return None
