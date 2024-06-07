62babd2c65695ba3a8f5244548b65b0291ef2217
endymion18 <taciturnasilentia@gmail.com>
2024-06-07 18:28:05 +0500

    feat: add CORS

--- api/main.py
+++ api/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from main_router import router as main_router
 from admin_router import router as admin_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "http://localhost",
+    "https://ivr-urfu.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 app.include_router(main_router)
 app.include_router(admin_router)
